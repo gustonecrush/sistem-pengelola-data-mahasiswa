18657e71e4783ff1eb5e45d5a7298d8f9bb28929
gustonecrush <codeboxezz@gmail.com>
2023-01-07 19:04:39 +0700

    started creating component

---
+++ components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../styles/Dashboard.module.css";
+
+function Dashboard() {
+  return (
+    <h1 className={styles.heading} style={{ fontWeight: "500" }}>
+      Dashboard
+    </h1>
+  );
+}
+
+export default Dashboard
---
+++ components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../styles/Search.module.css";
+
+function Search() {
+  return (
+    <h1 className={styles.heading} style={{ fontWeight: "500" }}>
+      Search
+    </h1>
+  );
+}
+
+export default Search
